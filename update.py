bca74a46a8b330ec2b17f51ae40c85cd44e3303a
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2023-09-01 04:44:38 +0900

    Updated to Ver. 306.0
    added +19*3 domain that is expected to be updated
    added update.py to automatically update the numbers

---
+++ update.py
@@ -0,0 +1,90 @@
+spare = 20
+n = int(input("Update Number: "))
+
+readme = f'''# List_Toki
+
+[**Current Version: {n}.0**](https://github.com/Mango-Clark/List_Toki/blob/main/List_Toki.txt)
+
+List_Toki is an AdBlock filter list for websites that provide online content for Korean viewers. It contains a set of filters designed to block specific elements on these websites, such as ad banners and pop-up windows, to provide a more streamlined and pleasant browsing experience.
+
+## Usage
+
+List_Toki is suggested to be used with AdBlock services such as ABP, uBO, and Brave browser, as well as the List-KR add-on, which is available on GitHub. To use the filter list, simply add the URL of the filter to your AdBlock service's custom filter settings.
+
+Here's the URL for the custom filter:
+
+```
+https://raw.githubusercontent.com/Mango-Clark/List_Toki/main/List_Toki.txt
+```
+
+### These filters can filter non-ad type in supported domains
+
+* [Fanboy's Annoyances](https://fanboy.co.nz/)
+
+## Supported Domains
+
+List_Toki includes filters for the following domains:
+
+* `newtoki{n}.com`
+* `manatoki{n}.net`
+* `booktoki{n}.com`
+
+Each domain has a separate set of filters that target specific elements on the website.
+
+## Diffrence between List-KR
+
+List-KR just hide ads so the blank space is left.
+
+Mean while List_Toki block ads and hides the blank space which makes user experience better.
+
+## Updating the Filters
+
+The List_Toki filter list is regularly updated to keep up with changes to the websites it targets. The version number for the filter list indicates the latest update in the format of `{{web number}}.{{changed number}}`.
+
+To ensure that you have the latest filters, it is suggested to update your AdBlock service's filter settings regularly, or manually update the filter list by downloading the latest version from GitHub. Since the website changes frequently please make sure this filter is updated.
+
+## Contributing
+
+If you have suggestions for improving the List_Toki filter list, or would like to report issues with the current filters, please feel free to open an issue or submit a pull request on the List_Toki GitHub repository. Contributions and feedback are always welcome!
+
+## Requesting New Domains
+
+If you want to request other domains, please report issues with site-support-request label.
+'''
+
+open("./README.md", "w").write(readme)
+
+list_toki = f'''! Title: List_Toki
+! Description: Adblock list for toki services.
+! Version: {n}.0
+! Expires: 12 hours
+! Homepage: https://github.com/Mango-Clark/List_Toki
+! Test environment: Brave browser
+! Note: For more convenience, use this filter list with List-KR(https://github.com/List-KR/List-KR) and other AdBlock services.(ABP, uBO, Brave browser...)
+
+newtoki{n}.com,booktoki{n}.com,manatoki{n}.net###hd_pop
+newtoki{n}.com,booktoki{n}.com,manatoki{n}.net#?#:has(> div > a[href*="linkbn.php"])
+newtoki{n}.com,booktoki{n}.com#?#:has(> a[href^="https://t.me/"])
+newtoki{n}.com,booktoki{n}.com###main-banner-view > div.col-15.col-md-9.col-sm-9
+newtoki{n}.com,booktoki{n}.com###id_mbv > div.col-15.col-md-9.col-sm-9
+
+'''
+for i in range(n + 1, n + spare):
+	list_toki += f'newtoki{i}.com,booktoki{i}.com,manatoki{i}.net,'
+list_toki = list_toki[:-1] + '###hd_pop\n'
+for i in range(n + 1, n + spare):
+	list_toki += f'newtoki{i}.com,booktoki{i}.com,manatoki{i}.net,'
+list_toki = list_toki[:-1] + '#?#:has(> div > a[href*="linkbn.php"])\n'
+
+for i in range(n + 1, n + spare):
+	list_toki += f'newtoki{i}.com,booktoki{i}.com,'
+list_toki = list_toki[:-1] + '#?#:has(> a[href^="https://t.me/"])\n'
+for i in range(n + 1, n + spare):
+	list_toki += f'newtoki{i}.com,booktoki{i}.com,'
+list_toki = list_toki[:-1] + '###main-banner-view > div.col-15.col-md-9.col-sm-9\n'
+for i in range(n + 1, n + spare):
+	list_toki += f'newtoki{i}.com,booktoki{i}.com,'
+list_toki = list_toki[:-1] + '###id_mbv > div.col-15.col-md-9.col-sm-9'
+
+open("./List_Toki.txt", "w").write(list_toki)
+print(list_toki)
